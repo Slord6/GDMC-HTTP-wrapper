da94a40d9048f94d10d45327229ae1f9085da765
Sam Lord <23644091+Slord6@users.noreply.github.com>
2022-12-16 22:57:45 +0000

    Remove Cyotek.Data.Nbt dep

--- GDMC-Debugging/Program.cs
+++ GDMC-Debugging/Program.cs
@@ -8,14 +8,13 @@ Func<string, Connection, string> announce = (string msg, Connection connection) 
 };
 
 Connection connection = new Connection();
-
 announce("Client connected", connection);
 
 for (int y = 65; y < 70; y++)
 {
     for (int x = 0; x < 5; x++)
     {
-        BiomePoint b = connection.GetBiomeSync(new Vec3Int(x, y, 0));
+        Block b = connection.GetBlockSync(new Vec3Int(x, y, 0));
         Console.WriteLine(b.ToString());
     }
 }
--- GDMCHttp/Data/BlockProperties.cs
+++ GDMCHttp/Data/BlockProperties.cs
@@ -1,5 +1,4 @@
-﻿using Cyotek.Data.Nbt;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
