5e31d7f9fe03deddc25bf66b217f30fc837f4ced
Sam Lord <23644091+Slord6@users.noreply.github.com>
2022-12-27 22:37:46 +0000

    Add weather command

---
+++ GDMCHttp/Commands/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GDMCHttp.Commands
+{
+    public class Weather : Command
+    {
+        public Weather(WeatherType type) : base("weather", new string[] {type.ToString().ToLower()})
+        {
+
+        }
+    }
+}
---
+++ GDMCHttp/Commands/WeatherType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GDMCHttp.Commands
+{
+    public enum WeatherType
+    {
+        Clear,
+        Rain,
+        Thunder
+    }
+}
